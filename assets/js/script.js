c6799c6ddcd6e82954fc796a1c31a9e9be19379d
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-07-02 08:17:35 -0700

    arrays, window prompt and confirm, if statements, and return

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,53 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+function generatePassword() {
+  var possibleArray = []
+  var passLength = parseInt(window.prompt('Number of characters (between 8 and 128)'));
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)){
+    return 'please select a valid number between 8 and 128'
+  }
+  if (window.confirm('would you like lower case letters?')) { 
+    possibleArray = possibleArray.concat(lowerCaseCharacters);
+  }
+  if (window.confirm('would you like upper case letters?')) { 
+    possibleArray = possibleArray.concat(upperCaseCharacters);
+  }
+  if (window.confirm('would you like numbers?')) { 
+    possibleArray = possibleArray.concat(numericCharacters);
+  }if (window.confirm('would you like special characters?')) { 
+    possibleArray = possibleArray.concat(specialCharacters);
+  }
+  if (possibleArray.length === 0){
+    return 'please choose at least one character type';
+  }
+  var results = []
+  for (var i = 0; i < passLength; i++){
+    var index = Math.floor(Math.random()*possibleArray.length);
+    var digit = possibleArray[index]
+    results.push(digit)
+  }
+  return results.join('')
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+var upperCaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(
+  ''
+);
+
+var lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz'.split(
+  ''
+);
+
+var numericCharacters = '0123456789'.split(
+  ''
+);
+
+var specialCharacters = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split(
+  ''
+);
+
+console.log(upperCaseCharacters, lowerCaseCharacters, numericCharacters, specialCharacters)
