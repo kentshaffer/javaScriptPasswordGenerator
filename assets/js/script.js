891f1deb61d7bdab82aa8c537ca2cba322067348
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-07-05 17:27:11 -0700

    updated window confirm messages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-function generatePassword() {
-  var possibleArray = []
-  var passLength = parseInt(window.prompt('Number of characters (between 8 and 128)'));
-  if (passLength < 8 || passLength > 128 || isNaN(passLength)){
-    return 'please select a valid number between 8 and 128'
-  }
-  if (window.confirm('would you like lower case letters?')) { 
-    possibleArray = possibleArray.concat(lowerCaseCharacters);
-  }
-  if (window.confirm('would you like upper case letters?')) { 
-    possibleArray = possibleArray.concat(upperCaseCharacters);
-  }
-  if (window.confirm('would you like numbers?')) { 
-    possibleArray = possibleArray.concat(numericCharacters);
-  }if (window.confirm('would you like special characters?')) { 
-    possibleArray = possibleArray.concat(specialCharacters);
-  }
-  if (possibleArray.length === 0){
-    return 'please choose at least one character type';
-  }
-  var results = []
-  for (var i = 0; i < passLength; i++){
-    var index = Math.floor(Math.random()*possibleArray.length);
-    var digit = possibleArray[index]
-    results.push(digit)
-  }
-  return results.join('')
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
 var upperCaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(
   ''
 );
@@ -58,4 +17,42 @@ var specialCharacters = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split(
   ''
 );
 
-console.log(upperCaseCharacters, lowerCaseCharacters, numericCharacters, specialCharacters)
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+function generatePassword() {
+  var possibleArray = []
+  var passLength = parseInt(window.prompt('Number of characters (between 8 and 128)'));
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)){
+    return 'Please Select A Valid Number Between 8 And 128'
+  }
+  if (window.confirm('Would you like lower case letters? ("OK" for yes or "Cancel" for no)')) { 
+    possibleArray = possibleArray.concat(lowerCaseCharacters);
+  }
+  if (window.confirm('Would you like upper case letters? ("OK" for yes or "Cancel" for no)')) { 
+    possibleArray = possibleArray.concat(upperCaseCharacters);
+  }
+  if (window.confirm('Would you like numbers? ("OK" for yes or "Cancel" for no)')) { 
+    possibleArray = possibleArray.concat(numericCharacters);
+  }
+  if (window.confirm('Would you like special characters? ("OK" for yes or "Cancel" for no)')) { 
+    possibleArray = possibleArray.concat(specialCharacters);
+  }
+  if (possibleArray.length === 0){
+    return 'Please Choose At Least One Character Type';
+  }
+  var results = []
+  for (var i = 0; i < passLength; i++){
+    var index = Math.floor(Math.random()*possibleArray.length);
+    var digit = possibleArray[index]
+    results.push(digit)
+  }
+  return results.join('')
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
